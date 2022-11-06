a472d14eadfb213b53e7d71f8440b7d7ef2cb873
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2022-11-06 21:54:37 +0100

    Final commit, to complete the tasks required in the project. Added the update and delete function.

--- controllers/flightController.js
+++ controllers/flightController.js
@@ -1,10 +1,10 @@
-const Flight = require("../models/Flight");
+const Flights = require("../models/Flight");
 const {v4: uuid} = require("uuid");
 
 //get all flights
 exports.getFlights = async (req, res) => {
     try {
-        const flights = Flight;
+        const flights = Flights;
         res.status(200).json({
             message: "All flights",
             flights: flights.exampleModel
@@ -18,11 +18,12 @@ exports.getFlights = async (req, res) => {
 
 exports.getFlight = async (req, res) => {
     try{
-        let id = req.param.id;
-        const flight = Flights.find((flight) => flight.id === id);
+        let id = req.params.id;
+      
+        const neewFlight = Flights.exampleModel.find((neewFlight) => neewFlight.id === id);
         res.status(200).json({
             message: "Flight found",
-            flight,
+            neewFlight : neewFlight,
         });
     } catch (err) {
         res.status(500).json({ message: err.message});
@@ -43,7 +44,7 @@ exports.createFlights = async (req, res) => {
         }
         
 
-        Flight.exampleModel.push(newFlight);       
+        Flights.exampleModel.push(newFlight);       
         
         
         res.status(201).json({
@@ -57,7 +58,36 @@ exports.createFlights = async (req, res) => {
 
 //update flight data
 
-exports
+exports.updateFlight = async (req, res) => {
+    try{
+        let id = req.params.id;
+        const flight = Flights.exampleModel.find((flight) => flight.id ===id);
+        const {title, time, price, date} = await req.body;
+        flight.title = title;
+        flight.time = time;
+        flight.price = price;
+        flight.date = date;
+        res.status(200).json({
+            message: "Flight updated",
+            flight: flight,
+    });
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
+};
+
+exports.deleteFlight = async(req, res) => {
+    try{
+        let id = req.params.id;
+        const flight = Flights.exampleModel.find((flight) => flight.id === id);
+        Flights.exampleModel.splice(Flights.exampleModel.indexOf(flight), 1);
+
+        res.send(`Flight record with id number ${id} deleted`);
+    } catch (err){
+        res.status(500).json({message: err.message});
+    }
+
+};
 
 exports.example = (req, res) => {
     console.log("example")
--- routes/flightRoute.js
+++ routes/flightRoute.js
@@ -5,8 +5,10 @@ const controller = require('../controllers/flightController');
 
 router.get('/', controller.example);
 router.get('/flight', controller.getFlights);
-router.get('/flight', controller.getFlight);
+router.get('/flight/:id', controller.getFlight);
 router.post('/flight', controller.createFlights);
+router.put('/flight/:id', controller.updateFlight);
+router.delete('/flight/:id', controller.deleteFlight);
 
 module.exports = router;
 
